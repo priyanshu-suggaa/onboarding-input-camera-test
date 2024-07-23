2124b2365a4b52b41abcafaf91c8e914c5278219
priyanshu-suggaa <priyanshu@suggaa.in>
2024-07-23 14:36:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('fileButton').addEventListener('click', function() {
+    document.getElementById('fileInput').click();
+});
+
+document.getElementById('cameraButton').addEventListener('click', function() {
+    document.getElementById('cameraInput').click();
+});
